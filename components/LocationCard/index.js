b77f817147ca9ff8d8f9756bfa106b3fc3e4a90d
Doreen Schwurack <doreen.schwurack@gmx.de>
2026-06-11 22:14:00 +0200

    created LocationCard inlc. first styling

---
+++ components/LocationCard/index.js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import styled from "styled-components";
+
+export default function LocationCard({ location }) {
+  const { image, name, category } = location;
+
+  return (
+    <StyledCard>
+      <h2>{name}</h2>
+      <p>{category}</p>
+      <StyledImageContainer>
+        <StyledImage src={image} alt={name} fill />
+      </StyledImageContainer>
+    </StyledCard>
+  );
+}
+
+const StyledCard = styled.div`
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  overflow: hidden;
+  flex: 1 1 300px;
+  background-color: white;
+  box-shadow:
+    0 1px 1px hsl(0deg 0% 0% / 0.075),
+    0 2px 2px hsl(0deg 0% 0% / 0.075),
+    0 4px 4px hsl(0deg 0% 0% / 0.075),
+    0 8px 8px hsl(0deg 0% 0% / 0.075),
+    0 16px 16px hsl(0deg 0% 0% / 0.075);
+`;
+
+const StyledImageContainer = styled.div`
+  position: relative;
+  height: 400px;
+`;
+
+const StyledImage = styled(Image)`
+  object-fit: cover;
+  pointer-events: none;
+`;
