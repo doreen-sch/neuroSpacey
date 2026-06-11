022ffa061a527be38276459e130faed3508bf0c2
Doreen Schwurack <doreen.schwurack@gmx.de>
2026-06-11 16:52:13 +0200

    added data

---
+++ db/data.js
@@ -0,0 +1,62 @@
+const data = [
+  {
+    name: "Stille Stunde Rewe",
+    category: "Einkaufen",
+    description:
+      "Stille Stunde im Rewe. Jeden Mittwoch von 16:30 Uhr bis 18:30 Uhr.",
+    address: {
+      Straße: "Petersstraße",
+      Hausnummer: "33 - 35",
+      Postleitzahl: "04109",
+      Stadt: "Leipzig",
+    },
+    coordinates: { lat: 0, lng: 0 },
+    image: "",
+    isApproved: true,
+  },
+  {
+    name: "Stille Stunde Decathlon",
+    category: "Einkaufen",
+    description:
+      "Stille Stunde an jedem ersten Montag im Monat von 15:00 Uhr bis 16:00 Uhr. Lautes Testen der Produkte soll vermieden werden.",
+    address: {
+      Straße: "Petersstraße",
+      Hausnummer: "36 - 44",
+      Postleitzahl: "04109",
+      Stadt: "Leipzig",
+    },
+    coordinates: { lat: 0, lng: 0 },
+    image: "",
+    isApproved: true,
+  },
+  {
+    name: "Stille Stunde Ikea",
+    category: "Einkaufen",
+    description:
+      "Stille Stunde in jedem Ikea Einrichtngshaus, deutschlandweit, an jedem Mittwich von 17:00 Uhr bis 19:00 Uhr.",
+    address: {
+      Straße: "Petersstraße",
+      Hausnummer: "36 - 44",
+      Postleitzahl: "04109",
+      Stadt: "Leipzig",
+    },
+    coordinates: { lat: 0, lng: 0 },
+    image: "",
+    isApproved: true,
+  },
+  {
+    name: "Friseur",
+    category: "???",
+    description:
+      "Ruhige Arbeitsathmosphäre, dimmbares Licht, visuell abgetrennter Bereich. Chat- oder Chill-Mood einfach über ein Schild am Platz anzeigen. Bei 'Chill' wird im gesamten Salon möglichst wenig gesprochen.",
+    address: {
+      Straße: "Löhrstraße",
+      Hausnummer: "14",
+      Postleitzahl: "04105",
+      Stadt: "Leipzig",
+    },
+    coordinates: { lat: 0, lng: 0 },
+    image: "",
+    isApproved: true,
+  },
+];
