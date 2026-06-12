b9d0508f7e8411d00faf1d8d6e208db39948804b
Doreen <doreen.schwurack@gmx.de>
2026-06-12 10:42:35 +0200

    Apply suggestion from @FelineHuhn
    Co-authored-by: FelineHuhn <112863528+FelineHuhn@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import LocationList from "@/components/LocationList";
 
 export default function HomePage() {
-  const { data: locations, error } = useSWR("/api/locations");
+  const { data: locations, error, isLoading } = useSWR("/api/locations");
 
   if (error) {
     return <h1>Oops… something went wrong.</h1>;
