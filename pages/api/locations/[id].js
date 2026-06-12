58bb40f30192e3cb4d6f86759d853bfba00052a6
Doreen Schwurack <doreen.schwurack@gmx.de>
2026-06-12 12:52:58 +0200

    fetched single location by ID and handled error messages

---
+++ pages/api/locations/[id].js
@@ -0,0 +1,21 @@
+import dbConnect from "@/db/connect";
+import Location from "@/db/models/Location";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  try {
+    if (request.method === "GET") {
+      const location = await Location.findById(id);
+      if (!location) {
+        return response.status(404).json({ status: "Not found!" });
+      }
+      return response.status(200).json(location);
+    }
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({ status: "Internal Server Error." });
+  }
+  return response.status(405).json({ status: "Method not allowed." });
+}
